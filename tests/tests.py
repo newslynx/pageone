917b1fadccd3cd1548dc13559c6d0096d18ac778
abelsonlive <brianabelson@gmail.com>
2015-10-06 16:29:09 -0400

    update tests

--- tests/tests.py
+++ tests/tests.py
@@ -8,20 +8,14 @@ FIXTURES_DIR = os.path.join(TEST_DIR, 'fixtures')
 p = r'(.*projects\.propublica\.org/.*)|(.*/article/.*)'
 
 
-for link in pageone.get('https://www.propublica.org/', pattern=p):
-    print link
-
-
 class Tests(unittest.TestCase):
 
     def test_link_stats(self):
         """
         TK: better tests, I'm just checking it runs various ways. for now:
         """
-        for link in pageone.get('https://www.propublica.org/', uniq=True):
+        for link in pageone.get('https://www.propublica.org/', pattern=p):
             assert('url' in link)
 
-        for link in pageone.get('https://www.npr.org', pattern=".*/article/.*", visible_only=True):
+        for link in pageone.get('https://www.npr.org', pattern=p, visible_only=True):
             assert(link['visible'])
-
-
